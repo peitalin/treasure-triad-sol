1845745bcbb6acd458f40623cd0430ed5f8f8d2d
Peita <n6378056@gmail.com>
2022-03-27 15:26:15 +1000

    add cardstats formatted for deploy scripts

---
+++ triad_sol/contracts/cardStats.ts
@@ -0,0 +1,539 @@
+
+var CARDS_T1 = [
+	// 'honeycomb':
+  {
+    "treasureId": 95,
+    "cardInfo": {
+	    "north": 2,
+	    "east": 8,
+	    "south": 8,
+	    "west": 4,
+	    "category": TreasureCategory.LEATHERWORKING
+    }
+	},
+	// 'grin':
+  {
+    "treasureId": 95,
+    "cardInfo": {
+      "north": 8,
+      "east": 2,
+      "south": 8,
+      "west": 2,
+      "category": TreasureCategory.ARCANA
+    }
+	},
+  // 'bottomless elixer':
+  {
+    "treasureId": 51,
+    "cardInfo": {
+	    "north": 4,
+	    "east": 2,
+	    "south": 7,
+	    "west": 7,
+	    "category": TreasureCategory.ENCHANTER
+    }
+	},
+	// 'cap of invisibility':
+  {
+    "treasureId": 52,
+    "cardInfo": {
+	    "north": 2,
+	    "east": 8,
+	    "south": 5,
+	    "west": 3,
+	    "category": TreasureCategory.SMITHING
+    }
+	},
+	// 'ancient relic':
+  {
+    "treasureId": 39,
+    "cardInfo": {
+	    "north": 7,
+	    "east": 7,
+	    "south": 5,
+	    "west": 3,
+	    "category": TreasureCategory.BREWING
+    }
+	},
+	// 'castle':
+  {
+    "treasureId": 54,
+    "cardInfo": {
+	    "north": 3,
+	    "east": 6,
+	    "south": 5,
+	    "west": 7,
+	    "category": TreasureCategory.ALCHEMY
+    }
+	}
+]
+
+
+var CARD_STATS_T2 = [
+	// 'thread of divine silk':
+  {
+    "treasureId": 161,
+    "cardInfo": {
+	    "north": 7,
+	    "east": 5,
+	    "south": 3,
+	    "west": 4,
+	    "category": TreasureCategory.ARCANA
+    }
+	},
+	// 'immovable stone':
+  {
+    "treasureId": 98,
+    "cardInfo": {
+	    "north": 5,
+	    "east": 6,
+	    "south": 5,
+	    "west": 6,
+	    "category": TreasureCategory.ENCHANTER
+    }
+	},
+	// 'snow white feather':
+  {
+    "treasureId": 153,
+    "cardInfo": {
+	    "north": 6,
+	    "east": 2,
+	    "south": 7,
+	    "west": 3,
+	    "category": TreasureCategory.ENCHANTER
+    }
+	},
+	// 'ivory breast pin':
+  {
+    "treasureId": 99,
+    "cardInfo": {
+	    "north": 3,
+	    "east": 7,
+	    "south": 3,
+	    "west": 6,
+	    "category": TreasureCategory.ALCHEMY
+    }
+	},
+	// 'military stipend':
+  {
+    "treasureId": 104,
+    "cardInfo": {
+	    "north": 7,
+	    "east": 4,
+	    "south": 4,
+	    "west": 4,
+	    "category": TreasureCategory.SMITHING
+    }
+	},
+	// 'bait for monsters':
+  {
+    "treasureId": 47,
+    "cardInfo": {
+	    "north": 7,
+	    "east": 3,
+	    "south": 6,
+	    "west": 1,
+	    "category": TreasureCategory.BREWING
+    }
+	},
+	// 'mollusk shell':
+  {
+    "treasureId": 105,
+    "cardInfo": {
+	    "north": 1,
+	    "east": 6,
+	    "south": 4,
+	    "west": 7,
+	    "category": TreasureCategory.LEATHERWORKING
+    }
+	},
+	// 'red feather':
+  {
+    "treasureId": 132,
+    "cardInfo": {
+	    "north": 2,
+	    "east": 7,
+	    "south": 6,
+	    "west": 3,
+	    "category": TreasureCategory.BREWING
+    }
+	},
+	// 'divine hourglass':
+  {
+    "treasureId": 74,
+    "cardInfo": {
+	    "north": 4,
+	    "east": 6,
+	    "south": 2,
+	    "west": 7,
+	    "category": TreasureCategory.ARCANA
+    }
+	},
+	// 'bag of mushrooms':
+  {
+    "treasureId": 46,
+    "cardInfo": {
+	    "north": 6,
+	    "east": 5,
+	    "south": 6,
+	    "west": 5,
+	    "category": TreasureCategory.LEATHERWORKING
+    }
+	},
+	// 'carriage':
+  {
+    "treasureId": 53,
+    "cardInfo": {
+	    "north": 2,
+	    "east": 3,
+	    "south": 6,
+	    "west": 7,
+	    "category": TreasureCategory.SMITHING
+    }
+	}
+]
+
+
+var CARD_STATS_T3 = [
+	// 'small bird':
+  {
+    "treasureId": 152,
+    "cardInfo": {
+	    "north": 7,
+	    "east": 2,
+	    "south": 3,
+	    "west": 5,
+	    "category": TreasureCategory.LEATHERWORKING
+    }
+	},
+	// 'unbreakable pocketwatch':
+  {
+    "treasureId": 162,
+    "cardInfo": {
+	    "north": 6,
+	    "east": 2,
+	    "south": 6,
+	    "west": 3,
+	    "category": TreasureCategory.ARCANA
+    }
+	},
+	// 'cow':
+  {
+    "treasureId": 72,
+    "cardInfo": {
+	    "north": 4,
+	    "east": 4,
+	    "south": 7,
+	    "west": 2,
+	    "category": TreasureCategory.LEATHERWORKING
+    }
+	},
+	// 'divine mask':
+  {
+    "treasureId": 75,
+    "cardInfo": {
+	    "north": 5,
+	    "east": 6,
+	    "south": 5,
+	    "west": 3,
+	    "category": TreasureCategory.ARCANA
+    }
+	},
+	// 'favor from the gods':
+  {
+    "treasureId": 82,
+    "cardInfo": {
+	    "north": 5,
+	    "east": 6,
+	    "south": 3,
+	    "west": 5,
+	    "category": TreasureCategory.BREWING
+    }
+	},
+	// 'score of ivory':
+  {
+    "treasureId": 99,
+    "cardInfo": {
+	    "north": 7,
+	    "east": 1,
+	    "south": 5,
+	    "west": 2,
+	    "category": TreasureCategory.ALCHEMY
+    }
+	},
+	// 'framed butterfly':
+  {
+    "treasureId": 91,
+    "cardInfo": {
+	    "north": 7,
+	    "east": 5,
+	    "south": 3,
+	    "west": 1,
+	    "category": TreasureCategory.ENCHANTER
+    }
+	},
+	// 'pot of gold':
+  {
+    "treasureId": 116,
+    "cardInfo": {
+	    "north": 4,
+	    "east": 5,
+	    "south": 5,
+	    "west": 5,
+	    "category": TreasureCategory.SMITHING
+    }
+	},
+	// 'common bead':
+  {
+    "treasureId": 68,
+    "cardInfo": {
+	    "north": 1,
+	    "east": 2,
+	    "south": 6,
+	    "west": 6,
+	    "category": TreasureCategory.ALCHEMY
+    }
+	},
+	// 'jar of fairies':
+  {
+    "treasureId": 100,
+    "cardInfo": {
+	    "north": 6,
+	    "east": 6,
+	    "south": 3,
+	    "west": 2,
+	    "category": TreasureCategory.ENCHANTER
+    }
+	}
+]
+
+
+var CARD_STATS_T4 = [
+	// 'witches broom':
+  {
+    "treasureId": 164,
+    "cardInfo": {
+	    "north": 4,
+	    "east": 4,
+	    "south": 5,
+	    "west": 2,
+	    "category": TreasureCategory.BREWING
+    }
+	},
+	// 'green rupee':
+  {
+    "treasureId": 94,
+    "cardInfo": {
+	    "north": 5,
+	    "east": 2,
+	    "south": 5,
+	    "west": 3,
+	    "category": TreasureCategory.ALCHEMY
+    }
+	},
+	// 'lumber':
+  {
+    "treasureId": 103,
+    "cardInfo": {
+	    "north": 5,
+	    "east": 1,
+	    "south": 3,
+	    "west": 5,
+	    "category": TreasureCategory.SMITHING
+    }
+	},
+	// 'ox':
+  {
+    "treasureId": 114,
+    "cardInfo": {
+	    "north": 5,
+	    "east": 5,
+	    "south": 4,
+	    "west": 3,
+	    "category": TreasureCategory.SMITHING
+    }
+	},
+	// 'donkey':
+  {
+    "treasureId": 76,
+    "cardInfo": {
+	    "north": 5,
+	    "east": 5,
+	    "south": 5,
+	    "west": 5,
+	    "category": TreasureCategory.LEATHERWORKING
+    }
+	},
+	// 'common feather':
+  {
+    "treasureId": 69,
+    "cardInfo": {
+	    "north": 6,
+	    "east": 1,
+	    "south": 4,
+	    "west": 3,
+	    "category": TreasureCategory.ENCHANTER
+    }
+	},
+	// 'grain':
+  {
+    "treasureId": 93,
+    "cardInfo": {
+	    "north": 5,
+	    "east": 3,
+	    "south": 3,
+	    "west": 4,
+	    "category": TreasureCategory.BREWING
+    }
+	},
+	// 'common relic':
+  {
+    "treasureId": 71,
+    "cardInfo": {
+	    "north": 6,
+	    "east": 2,
+	    "south": 2,
+	    "west": 3,
+	    "category": TreasureCategory.ARCANA
+    }
+	},
+	// 'blue rupee':
+  {
+    "treasureId": 49,
+    "cardInfo": {
+	    "north": 6,
+	    "east": 3,
+	    "south": 1,
+	    "west": 3,
+	    "category": TreasureCategory.ENCHANTER
+    }
+	}
+]
+
+var CARD_STATS_T5 = [
+	// 'half penny':
+  {
+    "treasureId": 96,
+    "cardInfo": {
+	    "north": 4,
+	    "east": 2,
+	    "south": 4,
+	    "west": 3,
+	    "category": TreasureCategory.LEATHERWORKING
+    }
+	},
+	// 'diamond':
+  {
+    "treasureId": 73,
+    "cardInfo": {
+	    "north": 2,
+	    "east": 1,
+	    "south": 6,
+	    "west": 1,
+	    "category": TreasureCategory.ALCHEMY
+    }
+	},
+	// 'dragon tail':
+  {
+    "treasureId": 77,
+    "cardInfo": {
+	    "north": 3,
+	    "east": 5,
+	    "south": 2,
+	    "west": 1,
+	    "category": TreasureCategory.BREWING
+    }
+	},
+	// 'gold coin':
+  {
+    "treasureId": 92,
+    "cardInfo": {
+	    "north": 1,
+	    "east": 5,
+	    "south": 4,
+	    "west": 1,
+	    "category": TreasureCategory.SMITHING
+    }
+	},
+	// 'beetle wings':
+  {
+    "treasureId": 48,
+    "cardInfo": {
+	    "north": 2,
+	    "east": 1,
+	    "south": 4,
+	    "west": 4,
+	    "category": TreasureCategory.BREWING
+    }
+	},
+	// 'silver coin':
+  {
+    "treasureId": 151,
+    "cardInfo": {
+	    "north": 2,
+	    "east": 3,
+	    "south": 1,
+	    "west": 5,
+	    "category": TreasureCategory.ENCHANTER
+    }
+	},
+	// 'pearl':
+  {
+    "treasureId": 115,
+    "cardInfo": {
+	    "north": 6,
+	    "east": 1,
+	    "south": 1,
+	    "west": 2,
+	    "category": TreasureCategory.ARCANA
+    }
+	},
+	// 'red rupee':
+  {
+    "treasureId": 133,
+    "cardInfo": {
+	    "north": 1,
+	    "east": 3,
+	    "south": 3,
+	    "west": 5,
+	    "category": TreasureCategory.SMITHING
+    }
+	},
+	// 'emerald':
+  {
+    "treasureId": 79,
+    "cardInfo": {
+      "north": 5,
+      "east": 1,
+      "south": 1,
+      "west": 3,
+      "category": TreasureCategory.ALCHEMY
+    }
+	},
+	// 'quarter penny':
+  {
+    "treasureId": 117,
+    "cardInfo": {
+      "north": 1,
+      "east": 4,
+      "south": 1,
+      "west": 5,
+      "category": TreasureCategory.LEATHERWORKING
+    }
+	},
+	// 'none': {
+	// 		"treasureId": 0,
+	//     "north": ' ',
+	//     "east": ' ',
+	//     "south": ' ',
+	//     "west": ' ',
+	//     "category": "none",
+	// }
+]
+
+
+
+
+
+
+
